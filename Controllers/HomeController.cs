65525458033a4316ca375469b812aa5bf473ad69
Иван <ikotskiy@gmail.com>
2024-05-02 19:06:36 +0700

    Отрисовка котика с помощью api.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace OsiteNew.Controllers {
     public class HomeController : Controller {
-        public IActionResult HomePage() {
+        public async Task<IActionResult> HomePage() {
+            using(HttpClient client = new HttpClient()) {
+                HttpResponseMessage response = await client.GetAsync("https://api.thecatapi.com/v1/images/search");
+                if(response.IsSuccessStatusCode) {
+                    string json = await response.Content.ReadAsStringAsync();
+                    dynamic data = JsonConvert.DeserializeObject(json);
+                    string imageUrl = data[0].url;
 
+                    ViewBag.ImageUrl = imageUrl;
+                }
+            }
             return View();
         }
     }
